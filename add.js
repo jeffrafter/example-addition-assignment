f6e79650f1c7c7c496721aa8ff492df225739a7f
Jeff Rafter <jeffrafter@github.com>
2019-09-25 16:59:05 -0700

    Update add.js

--- add.js
+++ add.js
@@ -2,4 +2,5 @@ module.exports = (left, right) => {
   // Write a function that adds the left number to the right number
   // and returns it. If either of the inputs are not valid numbers
   // throw an Error
+  return 4
 }
